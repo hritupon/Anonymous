6bf551fca71a929c81b6e582b617fb03038936ae
hritupon <hritupon.saikia@gmail.com>
2019-05-12 22:08:24 +0530

    First code

--- app/src/main/java/com/anonymous/LoginActivity.java
+++ app/src/main/java/com/anonymous/LoginActivity.java
@@ -1,30 +1,45 @@
 package com.anonymous;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.anonymous.Utils.Util;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+
     private Button loginButton;
     private Button phoneLoginButton;
     private EditText userEmail;
     private EditText userPassword;
     private TextView needNewAccountLink;
     private TextView forgotPasswordLink;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
         initilazeFields();
 
         needNewAccountLink.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +48,52 @@ public class LoginActivity extends AppCompatActivity {
                 sendUserToRegisterActivity();
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allowUserToLogin();
+            }
+        });
+    }
+
+    private void allowUserToLogin() {
+        final Animation shake = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.shake);
+
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)||!Util.isEmailValid(email)) {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT);
+            findViewById(R.id.register_email).startAnimation(shake);
+
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT);
+            findViewById(R.id.register_password).startAnimation(shake);
+
+            return;
+        }
+        progressDialog.setTitle("Sing In");
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            sendUserToMainActivity();
+                            Toast.makeText(LoginActivity.this, "Login Successful...", Toast.LENGTH_SHORT);
+                            progressDialog.dismiss();
+                        } else {
+                            String message = task.getException().toString();
+                            Toast.makeText(LoginActivity.this, "Login Failed."+message, Toast.LENGTH_SHORT);
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
     }
 
     private void initilazeFields() {
@@ -42,6 +103,8 @@ public class LoginActivity extends AppCompatActivity {
         userPassword = (EditText)findViewById(R.id.login_password);
         needNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
         forgotPasswordLink = (TextView)findViewById(R.id.forget_password_link);
+        progressDialog = new ProgressDialog(this);
+
     }
 
     @Override
--- app/src/main/java/com/anonymous/MainActivity.java
+++ app/src/main/java/com/anonymous/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter myTabsAccessorAdapter;
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         mToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -48,5 +54,28 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.main_logout_options){
+           mAuth.signOut();
+           sendUserToLoginActivity();
+        }
+        else if(item.getItemId()==R.id.main_settings_options){
+
+        }
+        if(item.getItemId()==R.id.main_find_friends_options){
+
+        }
+        return true;
     }
 }
