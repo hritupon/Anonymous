a9363e2024d0c504a096d5edc0ec05142a18c48d
hritupon <hritupon.saikia@gmail.com>
2019-05-12 21:45:36 +0530

    First code

--- app/src/main/java/com/anonymous/RegisterActivity.java
+++ app/src/main/java/com/anonymous/RegisterActivity.java
@@ -1,12 +1,27 @@
 package com.anonymous;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anonymous.Utils.Util;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,11 +29,16 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText userPassword;
     private Button createAccount;
     private TextView alreadyHaveAccountLink;
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+
         initilazeFields();
         alreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +46,56 @@ public class RegisterActivity extends AppCompatActivity {
                 sendUserToLoginActivity();
             }
         });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewAccount();
+            }
+        });
+    }
+
+    private void createNewAccount() {
+        final Animation shake = AnimationUtils.loadAnimation(RegisterActivity.this, R.anim.shake);
+
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)||!Util.isEmailValid(email)) {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT);
+            findViewById(R.id.register_email).startAnimation(shake);
+
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT);
+            findViewById(R.id.register_password).startAnimation(shake);
+
+            return;
+        }
+        progressDialog.setTitle("Creating New Account");
+        progressDialog.setMessage("Please wait, while we are creating new account for your...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Account created successfully...", Toast.LENGTH_LONG);
+                            sendUserToLoginActivity();
+
+                        } else {
+                            progressDialog.dismiss();
+                            String message = task.getException().toString();
+                            Toast.makeText(RegisterActivity.this, "Error: "+message, Toast.LENGTH_LONG);
+                            findViewById(R.id.register_password).startAnimation(shake);
+                        }
+                    }
+             }
+        );
+
     }
 
     private void initilazeFields() {
@@ -33,11 +103,15 @@ public class RegisterActivity extends AppCompatActivity {
         userEmail = (EditText)findViewById(R.id.register_email);
         userPassword = (EditText)findViewById(R.id.register_password);
         alreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+        progressDialog = new ProgressDialog(this);
     }
 
     private void sendUserToLoginActivity() {
         Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(loginIntent);
     }
+
+
+
 
 }
